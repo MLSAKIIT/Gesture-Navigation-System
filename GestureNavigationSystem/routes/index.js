a54a931e2992e822672ac117056b16ffa9fa6651
Kartik Agarwal <Kartik.agarwal.192.168@gmail.com>
2021-10-12 01:41:49 +0530

    Integrated Frontend with Backend

--- GestureNavigationSystem/routes/index.js
+++ GestureNavigationSystem/routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+/* GET about page. */
+router.get('/About', function(req, res, next) {
+  res.render('about');
+});
+
+/* GET viewMore1 page. */
+router.get('/viewMore1', function(req, res, next) {
+  res.render('viewMore1');
+});
+
+/* GET viewMore2 page. */
+router.get('/viewMore2', function(req, res, next) {
+  res.render('viewMore2');
+});
+
+/* GET contact page. */
+router.get('/ContactUs', function(req, res, next) {
+  res.render('contact');
 });
 
 module.exports = router;
