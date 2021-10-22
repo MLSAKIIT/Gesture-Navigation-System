6784793264aabc5940c8cfa05c70ba62bfb29acb
AyushSinha29 <55970505+AyushSinha29@users.noreply.github.com>
2021-10-23 00:42:33 +0530

    Update main.py

--- openCV/main.py
+++ openCV/main.py
@@ -100,10 +100,10 @@ while True:
                                         (20, 60), cv2.FONT_HERSHEY_PLAIN, 1, 2)    
                     
                 # Checking scroll down
-                ScrollDistUp = math.sqrt(((hand.landmark[12].x - hand.landmark[11].x) * w) ** 2 + (
+                ScrollDistDown = math.sqrt(((hand.landmark[12].x - hand.landmark[11].x) * w) ** 2 + (
                         (hand.landmark[12].y - hand.landmark[11].y) * h) ** 2)
 
-                if ScrollDistUp <5:
+                if ScrollDistDown <5:
                     frame = cv2.putText(frame, "Scrolling down",
                                         (20, 60), cv2.FONT_HERSHEY_PLAIN, 1, 2) 
                 # Checking zoom in
